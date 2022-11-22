e998e0f2d7a361e81b5d81e78a96ad29a7db3053
jashilko-hp <jashilko@gmail.com>
2022-11-22 23:37:21 +0300

    Меняем версию запуска Flask

--- main.py
+++ main.py
@@ -16,4 +16,4 @@ def index():
     return render_template('index.html', polygons=polygons, points=points, find_points=find_points)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
